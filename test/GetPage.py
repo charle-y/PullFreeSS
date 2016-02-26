7da4afa28c5128fa57678db71216ec65a1246be3
charle-y <yangdongchaos@gmail.com>
2016-02-26 15:01:43 +0800

    add new websit
    利用正则匹配，加入新网站

--- test/GetPage.py
+++ test/GetPage.py
@@ -11,7 +11,7 @@ class GetPage:
             'Connection': 'Keep-Alive',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-            'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0"'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'
         }
         
         req = urllib.request.Request(self.url,headers=user_agent)
--- test/main.py
+++ test/main.py
@@ -2,9 +2,11 @@ import codecs, re
 file = codecs.open('web.txt','r',"utf-8")
 content = file.read()
 from ishadowsocks import ishadowsocks
+from hishadowsocks import hishadowsocks
 from GetPage import GetPage
 SS=['  {\r\n"server" : "','",\r\n"server_port" : ',',\r\n"password" : "','",\r\n"method" : "','",\r\n"remarks" : "','"}\r\n']
-url='http://www.ishadowsocks.com/'
 
-happy = ishadowsocks(the_page=GetPage(url).get(),SS=SS)
-print(happy.get())
+
+happy = ishadowsocks(SS=SS)
+happy1 = hishadowsocks(SS=SS)
+print(happy1.get())
