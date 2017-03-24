50166a23cb48a2534f6e368d1776c2aca9a3a8ef
charle-y <yangdongchaos@gmail.com>
2017-03-24 22:36:16 +0800

    rewrite the code
    can read url and regular expression from re.ini

---
+++ src/HandleProtocal.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import re
+from GetPage import GetPage
+import json
+
+class ishadowsocks:
+    url = 'http://isx.yt/';
+    keyStr = ["IP Address:<.*?>(.*?)</","Port：(.*?)</","Password:<.*?>(.*?)</","Method:(.*?)</","IP Address:<span id=(.*?)>"]
+    SS = ["server", "server_port", "password", "method", "remarks"]
+    
+    url_List = []
+    keyStr_List = []
+    SS_List= []
+    config_len = -1
+    '''
+    
+    '''
+    def __init__(self):
+        try: 
+            self.readIni()
+        except:
+            self.creatIni()
+    def get(self):
+        ss = []
+        for i in range(self.config_len):    
+            self.url = self.url_List[i]
+            self.keyStr = self.keyStr_List[i]
+            self.SS = self.SS_List[i]
+            ss.extend(self.htmlWork())
+        #print(ss)
+        return ss
+    
+    def htmlWork(self):
+        Servers = [];
+        content = GetPage(self.url).get().decode("utf8")
+        b=[""]*5        
+        for i in range(5):
+            b[i]=re.findall(".*"+self.keyStr[i]+".*",content)
+        for j in range(len(b[0])):
+            server_dict = {};
+            for i in range(5):
+               server_dict[self.SS[i]] = b[i][j];
+            #print(server_dict)
+            Servers.append(server_dict);
+        #print(Servers)
+        #b[4] = 'unknown'
+        #print(Servers)
+        #Servers = %tuple(b)+",\r\n"self.SS[i]
+        return Servers
+        
+    def readIni(self):
+        config_len = -1
+        with open("re.ini", "r") as s:
+            j = json.load(s)
+            self.config_len = len(j["config"])
+            #print(self.config_len)
+            for i in range(self.config_len):
+                self.url_List.append(j["config"][i]["url"])
+                self.keyStr_List.append(j["config"][i]["keyStr"].split(';'))
+                self.SS_List.append(j["config"][i]["SS"].split(';'))
+                
+    def creatIni(self):        
+        jj = []
+        j = {}
+        j["url"] = self.url
+        j["keyStr"] =";".join(self.keyStr)
+        j["SS"] = ";".join(self.SS)
+        jj.append(j)
+        j1 = {}
+        j1["config"] = jj
+        with open("re.ini", "w") as s:
+            json.dump( j1, s, indent = 4)
+            
+if __name__=="__main__":
+    SS=["server", "server_port", "password", "method", "remarks", "auth", "timeout"]
+    happy = ishadowsocks()
+    with open("gui-config.json", "r") as s:
+        j = json.load(s)
+        j["configs"] = happy.get()
+        with open("gui-config.json", "w") as n_c:
+            try:
+                json.dump(j, n_c, indent = 4, separators=(',', ': '))
+                print("写入成功")
+            except:
+                print("写入失败")
+    #print(happy.get());
+
--- src/grep.py
+++ src/grep.py
@@ -1,47 +1,17 @@
 #! /usr/bin/python3.5
 #version 0.1
-import codecs #, os, sys
+import json
+from HandleProtocal import ishadowsocks
 
-from add_ishadowsocks import ishadowsocks
-from add_hishadowsocks import hishadowsocks
-
-try:
-  ori_file = codecs.open("gui-config.json", 'r',"utf-8")
-  ori_txt=ori_file.read()
-  ori_file.close()
-  Dress1=ori_txt[ori_txt.find("],"):]
-except:
-  Dress1=""
-
-Dresses = ['{\r\n"configs" : [\r\n','],\r\n  "strategy": null,\r\n  "index": 0,\r\n  "global": false,\
-  \r\n  "enabled": false,\r\n  "shareOverLan": false,\r\n  "isDefault": false,\r\n  "localPort": 8087,\
-  \r\n  "pacUrl": null,\r\n  "useOnlinePac": false,\r\n  "availabilityStatistics": false,\
-  \r\n  "autoCheckUpdate": true,\r\n  "isVerboseLogging": false,\r\n  "logViewer": null,\
-  \r\n  "useProxy": false,\r\n  "proxyServer": null,\r\n  "proxyPort": 0\r\n}']
-SS=['  {\r\n"server" : "','",\r\n"server_port" : ',',\r\n"password" : "','",\r\n"method" : "','",\r\n"remarks" : "','"}\r\n']
-
-Servers=""
-try:
-  happy = ishadowsocks(SS=SS)
-  Servers+=happy.get()
-except:
-  print("ishadowsocks is down!")
-
-try:
-  happy = hishadowsocks(SS=SS)
-  Servers+=happy.get()
-except:
-  print("hishadowsocks is down!")
-
-OUTfile=Dresses[0]+Servers
-if len(Dress1)<10:
-  OUTfile+=Dresses[1]
-else:
-  OUTfile+=Dress1
-print(OUTfile)
-
-output_file = codecs.open("gui-config.json", 'w+',"utf-8")
-output_file.write(OUTfile)
-output_file.close()
+happy = ishadowsocks()
+with open("gui-config.json", "r") as s:
+    j = json.load(s)
+    j["configs"] = happy.get()
+    with open("gui-config.json", "w") as n_c:
+        try:
+            json.dump(j, n_c, 'indent')
+            print("写入成功")
+        except:
+            print("写入失败")
 
 
